3d22367180d502ccb099d83c46bd2977ac921d7f
BoroDinS <sergey.borodin.lg@gmail.com>
2023-02-23 21:40:20 +0100

    refactor code, make decomposition, add styling using styled-components

---
+++ src/components/FeedbackInputInterface/FeedbackInputInterface.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ControlsContainer = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const Button = styled.button`
+  border-radius: 10px;
+`;
